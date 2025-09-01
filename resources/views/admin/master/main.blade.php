298b185ffedb525ae608ac0ccffd819db36eb8aa
farooqhalimzaigithub <farooqhalimzaigithub24@gmail.com>
2025-09-01 23:28:05 +0500

    new depoly

--- resources/views/admin/master/main.blade.php
+++ resources/views/admin/master/main.blade.php
@@ -7,7 +7,7 @@
 <body class=" layout-boxed">
     <!-- BEGIN LOADER -->
     <div id="load_screen">
-        <div class="loader">
+        <div class="loader"> 
             <div class="loader-content">
                 <div class="spinner-grow align-self-center"></div>
             </div>
