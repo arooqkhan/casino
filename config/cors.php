d7c12909ff4cc0de1dfb6cf85589eccfe0d98a1e
farooqhalimzaigithub <farooqhalimzaigithub24@gmail.com>
2025-10-03 10:38:03 +0500

    add cores error

--- config/cors.php
+++ config/cors.php
@@ -18,7 +18,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', 'http://localhost:8000'],
+    'allowed_origins' => ['http://localhost:5173', 'http://localhost:8000', 'https://megaspinn.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
